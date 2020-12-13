f34e6933e8f49906c2d21fb7776ac5d2feed7b0f
lucas-goldner <lucas.goldner@googlemail.com>
2020-12-13 18:38:34 +0100

    I made a function to find out this real FontName xD

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        VStack {
+            Button(action: {
+                for family in UIFont.familyNames.sorted() {
+                    let names = UIFont.fontNames(forFamilyName: family)
+                    print("Family: \(family) Font names: \(names)")
+                }
+            }, label: {
+                Text("Hey")
+            })
+            Text("L").font(Font.custom("CloisterBlack-Light", size: 33))
+        }
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 12 Pro Max")
     }
 }
