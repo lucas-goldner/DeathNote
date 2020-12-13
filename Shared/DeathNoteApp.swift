ba58c2b71cfedcda594b15152a7fc493150994fd
lucas-goldner <lucas.goldner@googlemail.com>
2020-12-13 18:09:05 +0100

    Initial Commit

---
+++ Shared/DeathNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeathNoteApp.swift
+//  Shared
+//
+//  Created by Lucas Goldner on 13.12.20.
+//
+
+import SwiftUI
+
+@main
+struct DeathNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
